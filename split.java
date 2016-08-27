6aacbda58259c9fab28136290bdfeec5f4ec558a
DIVYAPRABHA1602 <divyaprabha1602@gmail.com>
2016-08-27 20:50:15 +0530

    Create split.java

---
+++ split.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+public stativ void main(Strings[])
+{
+Scanner s=new Scanner(System.in);
+String line=s.nextLine();
+System.ou.println(line);
+String[] sp=line.split("\\.");
+String s1=sp[0];
+String s2=sp[1];
+System.out.println(s2);
+}
